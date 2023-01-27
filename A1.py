6faac11cf8a61e9377f694af95002fa8696a3add
RkWalia <120945936+RkWalia@users.noreply.github.com>
2023-01-27 22:46:38 +0530

    Add files via upload

---
+++ A1.py
@@ -0,0 +1,21 @@
+'''
+Assignment 6
+'''
+
+#Ques1
+
+def Perfect(num):
+	if num <= 0: 
+		print('Enter a natural no.')
+		return False
+
+	divs = []
+	for i in range(num,0,-1):
+		if num%i==0: divs.append(i)
+	if num*2==sum(divs): return True
+	else: return False
+
+a = input('Enter a number: ')
+a = int(input('Enter a number: '))
+if(Perfect(a)):print('Perfect')
+else: print('Not Perfect')
